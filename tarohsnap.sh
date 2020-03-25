b32ec840ef120caeeb369ab8b3f06c5a1f7fa647
Steven Osborne <sdo992@runbox.com>
2020-03-25 12:06:16 -0800

    Changed DELETE by removing  section to get it to actually work; changed  to , because why not?

--- tarohsnap.sh
+++ tarohsnap.sh
@@ -3,12 +3,12 @@
 # dcron daily bash Script
 
 # Variables
-tarsnap=/usr/local/bin/tarsnap
+ohsnap=/usr/local/bin/tarsnap
 holdLength=3 # Number of days
 logLocation=/home/$USER/.tarsnap/logs
 tlog=${logLocation}/tlog
 backToday=$(uname -n)-$(date +%Y%m%d)
-backOld=`$tarsnap --list-archives | grep ${backToday} | sort -r | sed 1,${holdLength}d | sort | xargs -n 1`
+backOld=`$ohsnap --list-archives | sort -r | sed 1,${holdLength}d | sort | xargs -n 1`
 backTargets="/path/to/backup1 /path/to/backup2"
 
 # Check if directory exists, if not, create it and the log file
@@ -21,12 +21,12 @@ if [ ! -e ${logLocation} ]; then
 fi
 
 # Backup if/else statement
-if ${tarsnap} --list-archives | sort | grep ${backToday} > /dev/null; then
+if ${ohsnap} --list-archives | sort | grep ${backToday} > /dev/null; then
     echo "**********" >> ${tlog}
     echo "BACKUP FOR $backToday ALREADY COMPLETED" >> ${tlog}
 else
     echo "BACKUP FOR $backToday INITIATED" >> ${tlog}
-    ${tarsnap} -cf ${backToday} $backTargets >> $tlog 2>&1
+    ${ohsnap} -cf ${backToday} $backTargets >> $tlog 2>&1
     echo "BACKUP COMPLETE for $backToday" >> ${tlog}
 fi
 
@@ -35,10 +35,11 @@ echo "ATTEMPTING TO DELETE BACKUPS OLDER THAN $holdLength DAYS" >> ${tlog}
 if [ -n "$backOld" ]; then 
     echo "Deleting the following backups: " >> ${tlog}
     echo "${backOld}" >> ${tlog}
-    $tarsnap --list-archives | grep $backToday | sort -r | sed 1,${holdLength}d | sort | xargs -n 1 $tarsnap -df >> ${tlog} 2>&1
+    $ohsnap --list-archives | sort -r | sed 1,${holdLength}d | sort | xargs -n 1 $ohsnap -df >> ${tlog} 2>&1
+    echo "DELETE SUCCESS - END OF DELETION SECTION" >> ${tlog}
 else
     echo "No backup files found to delete" >> ${tlog}
-    echo "END OF DELETION SUBROUTINE" >> ${tlog}
+    echo "END OF DELETION SECTION" >> ${tlog}
 fi
 
 # End of script
